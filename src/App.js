338b50b3672566274cead08f99babb59992ba42d
heathbaldwin <105754991+heathbaldwin@users.noreply.github.com>
2023-04-30 15:10:48 -0400

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,37 +1,44 @@
-import React from "react";
-import "./style.css";
-import { Navbar, Card } from "react-bootstrap"
-import "bootstrap/dist/css/bootstrap.min.css"
+import React from 'react';
+import './style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Card } from 'reactstrap';
 
 export default function App() {
   return (
     <div>
-  
-      <header className= "App-header">
-      <nav class="navbar bg-primary" data-bs-theme="dark">
-      <nav class="navbar bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      <img src="imgs/list-ol.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-      Decathlon World Rankings
-    </a>
-  </div>
-      </nav>
-      </nav>
-      </header>
-
-      <div class="card" style="width: 18rem;">
-  <img src="imgs/dwarner" class="card-img-top" alt="DameionWarner">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-
-    
-    
+      <Navbar>
+        <header className="App-header">
+          <nav class="navbar bg-primary" data-bs-theme="dark">
+            <nav class="navbar bg-body-tertiary">
+              <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                  <img
+                    src="imgs/list-ol.svg"
+                    alt="Logo"
+                    width="30"
+                    height="24"
+                    class="d-inline-block align-text-top"
+                  />
+                  Decathlon World Rankings
+                </a>
+              </div>
+            </nav>
+          </nav>
+        </header>
+      </Navbar>
+      <Card>
+        <div class="card" styles="width: 18rem;">
+          <img src="imgs/dwarner" class="card-img-top" alt="DameionWarner" />
+          <div class="card-body">
+            <h5 class="card-title">Decathlon fans from across the world...</h5>
+            <p class="card-text">
+              Welcome to the official world rankings page for the Decathlon
+              where we will be showing a list of the top decathletes from across
+              the globe throughout the 2023 season!
+            </p>
+          </div>
+        </div>
+      </Card>
     </div>
-    
   );
 }
