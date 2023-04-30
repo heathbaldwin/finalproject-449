e03fa4b7baf8c5a911235d9df1926530123164e9
heathbaldwin <105754991+heathbaldwin@users.noreply.github.com>
2023-04-30 14:54:46 -0400

    4 files modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React from "react";
 import "./style.css";
-import { Navbar } from "react-bootstrap"
+import { Navbar, Card } from "react-bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
 
 export default function App() {
   return (
     <div>
+  
       <header className= "App-header">
       <nav class="navbar bg-primary" data-bs-theme="dark">
       <nav class="navbar bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
-      <img src="imgs/list-ol.svg" alt="none" width="30" height="24" class="d-inline-block align-text-top"></img>
+      <img src="imgs/list-ol.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
       Decathlon World Rankings
     </a>
   </div>
-</nav>
-</nav>
- </header>
+      </nav>
+      </nav>
+      </header>
+
+      <div class="card" style="width: 18rem;">
+  <img src="imgs/dwarner" class="card-img-top" alt="DameionWarner">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+    
+    
     </div>
     
   );
