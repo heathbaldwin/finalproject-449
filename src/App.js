aa3195e1901d4cdc65e902a66e4077c98072adc2
heathbaldwin <105754991+heathbaldwin@users.noreply.github.com>
2023-04-30 14:15:16 -0400

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 export default function App() {
   return (
     <div>
-      <img src="..." class="img-fluid" alt="...">
+      <img src="imgs/dwarner" class="img-fluid" alt="damienwarner"></img>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
       <Button> Hi </Button>
